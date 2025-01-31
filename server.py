253085035587d971b29b726ecd8776ba025eed23
Soroush Nazemi <soroush.nazemi@gmail.com>
2025-01-30 21:25:53 -0600

    Error handling in server.py

--- server.py
+++ server.py
@@ -20,12 +20,16 @@ def emotion_detector_func():
     emotions = emotion_detector(statement)
     dominant_emotion = emotions["dominant_emotion"]
     
-    
-    formatted_response = (
-        f"For the given statement, the system response is <br />'anger': {emotions['anger']}, <br />"
-        f"'disgust': {emotions['disgust']}, <br />'fear': {emotions['fear']}, <br />'joy': {emotions['joy']} <br />"
-        f"and 'sadness': {emotions['sadness']}. <br />The dominant emotion is <b>{emotions['dominant_emotion']}</b>."
-    )
+    if emotions["dominant_emotion"] is None:
+        formatted_response = (
+            f"Invalid text! Please try again!."
+        )
+    else:
+        formatted_response = (
+            f"For the given statement, the system response is <br />'anger': {emotions['anger']}, <br />"
+            f"'disgust': {emotions['disgust']}, <br />'fear': {emotions['fear']}, <br />'joy': {emotions['joy']} <br />"
+            f"and 'sadness': {emotions['sadness']}. <br />The dominant emotion is <b>{emotions['dominant_emotion']}</b>."
+        )
     
     return (formatted_response)
 
