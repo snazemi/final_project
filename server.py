1a7955c8b0267cbe0b1ab07a364578d5a197b92e
Soroush Nazemi <soroush.nazemi@gmail.com>
2025-01-30 21:43:23 -0600

    updated server.py with pylint

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, jsonify, render_template
-
+"""
+server.py file providing emotion of the input text
+"""
+from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
@@ -7,26 +9,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    main index function
+    """
     return render_template('index.html')
 
 @app.route('/emotionDetector')
 def emotion_detector_func():
+    """
+    emotion detector function gets the emotion of the file and displays it out
+    """
     statement = request.args.get('textToAnalyze')
-    
     emotions = emotion_detector(statement)
-    
     if emotions["dominant_emotion"] is None:
-        formatted_response = (
-            f"Invalid text! Please try again!."
-        )
+        formatted_response = "Invalid text! Please try again!."
     else:
         formatted_response = (
-            f"For the given statement, the system response is <br />'anger': {emotions['anger']}, <br />"
-            f"'disgust': {emotions['disgust']}, <br />'fear': {emotions['fear']}, <br />'joy': {emotions['joy']} <br />"
-            f"and 'sadness': {emotions['sadness']}. <br />The dominant emotion is <b>{emotions['dominant_emotion']}</b>."
+            f"For the given statement, the system response is <br />"
+            f"'anger': {emotions['anger']}, <br />"
+            f"'disgust': {emotions['disgust']}, <br />"
+            f"'fear': {emotions['fear']}, <br />"
+            f"'joy': {emotions['joy']} <br />"
+            f"and 'sadness': {emotions['sadness']}. <br />"
+            f"The dominant emotion is <b>{emotions['dominant_emotion']}</b>."
         )
-    
-    return (formatted_response)
+    return formatted_response
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5001)
